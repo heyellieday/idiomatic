f804baf29e4f67d816a38a2dd4fa2daa7b884caf
Ellie Day <heyellieday@gmail.com>
2016-07-23 21:09:49 -0700

    update logo and responsiveness

--- app/assets/javascripts/components/header.js.jsx
+++ app/assets/javascripts/components/header.js.jsx
@@ -2,7 +2,7 @@ var Header = React.createClass({
   render: function() {
     return (
       <div id="Header">
-        <h1><img className="logo" alt="Idiomatic" src="https://files.slack.com/files-pri/T193VP4E5-F1UH45CRW/title.png"/></h1>
+        <h1><img className="logo" alt="Idiomatic" src="//i.imgur.com/PN7vH4x.png"/></h1>
         <h2 className="description">Learn idioms and sayings! Search for a word to find an idiom,
 learn its meaning, and watch a related YouTube video</h2>
       </div>
